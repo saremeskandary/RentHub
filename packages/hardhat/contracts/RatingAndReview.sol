68c0e18df9ec4753dd7d1c908a11f508101a5467
saremeskandary <saremeskandary@gmail.com>
2024-07-28 01:58:03 +0330

    NatSpec added

--- packages/hardhat/contracts/RatingAndReview.sol
+++ packages/hardhat/contracts/RatingAndReview.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title RatingAndReview
+ * @dev This contract allows users to submit and view reviews.
+ */
 contract RatingAndReview {
     struct Review {
         address reviewer;
@@ -14,6 +18,12 @@ contract RatingAndReview {
 
     event ReviewSubmitted(address indexed reviewer, address indexed reviewee, uint8 rating, string comment);
 
+    /**
+     * @notice Submit a review for a user
+     * @param _reviewee The address of the user being reviewed
+     * @param _rating The rating given to the user (1-5)
+     * @param _comment The comment provided by the reviewer
+     */
     function submitReview(address _reviewee, uint8 _rating, string memory _comment) external {
         require(_rating >= 1 && _rating <= 5, "Rating must be between 1 and 5");
         
@@ -30,10 +40,20 @@ contract RatingAndReview {
         emit ReviewSubmitted(msg.sender, _reviewee, _rating, _comment);
     }
 
+    /**
+     * @notice Get all reviews of a user
+     * @param _user The address of the user whose reviews are being retrieved
+     * @return An array of Review structs containing the user's reviews
+     */
     function getReviews(address _user) external view returns (Review[] memory) {
         return userReviews[_user];
     }
 
+    /**
+     * @notice Get the average rating of a user
+     * @param _user The address of the user whose average rating is being calculated
+     * @return The average rating of the user
+     */
     function getAverageRating(address _user) external view returns (uint8) {
         uint256 totalRating = 0;
         uint256 reviewCount = userReviews[_user].length;
--- packages/hardhat/contracts/RentalAgreement.sol
+++ packages/hardhat/contracts/RentalAgreement.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+/**
+ * @title RentalAgreement
+ * @dev This contract manages rental agreements for items using ERC721 tokens.
+ */
 contract RentalAgreement is ReentrancyGuard {
     struct Rental {
         address renter;
@@ -18,9 +22,16 @@ contract RentalAgreement is ReentrancyGuard {
     mapping(uint256 => Rental) public rentals;
     uint256 public rentalCount;
 
-    event RentalCreated(uint256 rentalId, address renter, address owner, uint256 itemId, uint256 startTime, uint256 endTime, uint256 price);
+    event RentalCreated(uint256 rentalId, address indexed renter, address indexed owner, uint256 itemId, uint256 startTime, uint256 endTime, uint256 price);
     event RentalEnded(uint256 rentalId);
 
+    /**
+     * @notice Create a rental agreement for an item
+     * @param _owner The address of the item's owner
+     * @param _itemId The ID of the item being rented
+     * @param _duration The duration of the rental in seconds
+     * @param _price The rental price
+     */
     function createRental(address _owner, uint256 _itemId, uint256 _duration, uint256 _price) external payable nonReentrant {
         require(msg.value == _price, "Incorrect payment amount");
 
@@ -38,6 +49,10 @@ contract RentalAgreement is ReentrancyGuard {
         emit RentalCreated(rentalId, msg.sender, _owner, _itemId, block.timestamp, block.timestamp + _duration, _price);
     }
 
+    /**
+     * @notice End a rental agreement
+     * @param _rentalId The ID of the rental agreement to be ended
+     */
     function endRental(uint256 _rentalId) external nonReentrant {
         Rental storage rental = rentals[_rentalId];
         require(msg.sender == rental.owner || msg.sender == rental.renter, "Not authorized");
