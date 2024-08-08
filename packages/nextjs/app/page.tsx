a3d1ec882379060c8c6878d3a420e6ace5158b6e
saremeskandary <saremeskandary@gmail.com>
2024-08-08 20:51:08 +0330

    doge theme added

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import '../styles/dogTheme.css';
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
