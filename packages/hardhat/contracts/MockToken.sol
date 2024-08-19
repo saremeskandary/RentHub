630a302758517b8088099efea06e146c80620134
saremeskandary <saremeskandary@gmail.com>
2024-08-19 12:31:47 +0330

    feat: Distributor_ROLE added

--- packages/hardhat/contracts/MockToken.sol
+++ packages/hardhat/contracts/MockToken.sol
@@ -4,14 +4,27 @@ pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { IAccessRestriction } from "./interfaces/IAccessRestriction.sol";
 
 contract MockToken is ERC20, ERC20Permit {
-    constructor()
+
+	IAccessRestriction public accessRestriction;
+
+
+
+ modifier onlyDistributor() {
+        accessRestriction.ifDistributor(msg.sender);
+        _;
+    }
+
+    constructor(address _accessRestriction)
         ERC20("MockToken", "MTK")
         ERC20Permit("MockToken")
-    {}
+    {
+        accessRestriction = IAccessRestriction(_accessRestriction);
+    }
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyDistributor{
         _mint(to, amount);
     }
 }
